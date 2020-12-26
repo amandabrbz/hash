d05f336e2bb3b1a9612daee8651eb95cdda8b51f
Amanda Barboza <amanda.barboza@mirumagency.com>
2020-12-26 13:30:58 -0300

    adiciona controladores de estado e um fetch mandando data dos inputs

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,24 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 const Form = () => {
+  const [amount, setAmount] = useState("");
+  const [installments, setInstallments] = useState("");
+  const [mdr, setMdr] = useState("");
+  const [smallInfo, setSmallInfo] = useState(false);
+
+  fetch("https://hash-front-test.herokuapp.com/", {
+    method: "POST",
+    body: JSON.stringify({
+      amount: amount,
+      installments: installments,
+      mdr: mdr,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then(function (response) {
+      if (response.ok) return response.json();
+      return Promise.reject(response);
+    })
+    .then(function (data) {
+      console.log(data);
+    })
+    .catch(function (error) {
+      alert("Não foi possível fazer o cálculo, tente novamente.");
+      console.warn("Não foi possível fazer o cálculo, tente novamente.", error);
+    });
+
   return (
     <div className="box__form">
       <h1 className="box__form--title">Simule sua Antecipação</h1>
       <form className="box__form--form" autoComplete="off">
         <fieldset>
-          <label htmlFor="sellValue">Informe o valor da venda*</label>
-          <input type="text" name="sellValue" id="sellValue" required />
+          <label htmlFor="amount">Informe o valor da venda*</label>
+          <input
+            type="text"
+            name="amount"
+            value={amount}
+            required
+            onChange={(e) => setAmount(e.target.value)}
+          />
         </fieldset>
         <fieldset>
-          <label htmlFor="quote">Em quantas parcelas*</label>
-          <input type="number" name="quote" id="quote" max="12" min="0" step="1" required />
-          <small>Máximo de 12 parcelas</small>
+          <label htmlFor="installments">Em quantas parcelas*</label>
+          <input
+            type="number"
+            name="installments"
+            max="12"
+            min="0"
+            step="1"
+            value={installments}
+            required
+            onChange={(e) => setInstallments(e.target.value)}
+            onClick={() => setSmallInfo(true)}
+          />
+          {smallInfo && <small>Máximo de 12 parcelas</small>}
         </fieldset>
         <fieldset>
-          <label htmlFor="percentage">Informe o percentual de MDR*</label>
-          <input type="text" name="percentage" id="percentage" required />
+          <label htmlFor="mdr">Informe o percentual de MDR*</label>
+          <input
+            type="text"
+            name="mdr"
+            value={mdr}
+            required
+            onChange={(e) => setMdr(e.target.value)}
+          />
         </fieldset>
+        <input type="submit" value="Enviar" />
       </form>
     </div>
   );
