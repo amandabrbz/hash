343f9dd5c724d4f16d4baef5d357ca313a269f89
Amanda Barboza <amanda.barboza@mirumagency.com>
2021-01-03 23:20:44 -0300

    ajusta visualizacao monetaria dos resultados conforme o calculo

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -4,20 +4,21 @@ import { Context } from '../Box/Box';
 
 const Results = () => {
   const {data} = useContext(Context);
+  
   return (
     <div className="box__result">
       <h2 className="box__result--title">Você receberá:</h2>
       <p className="box__result--text">
-        Amanhã: <strong>R$ {data[1] || 0},00</strong>
+        Amanhã: <strong>R$ {data[1] ? Number((data[1])).toLocaleString('pt-br') : 0},00</strong>
       </p>
       <p className="box__result--text">
-        Em 15 dias: <strong>R$ {data[15] || 0},00</strong>
+        Em 15 dias: <strong>R$ {data[15] ? Number((data[15])).toLocaleString('pt-br') : 0},00</strong>
       </p>
       <p className="box__result--text">
-        Em 30 dias: <strong>R$ {data[30] || 0},00</strong>
+        Em 30 dias: <strong>R$ {data[30] ? Number((data[30])).toLocaleString('pt-br') : 0},00</strong>
       </p>
       <p className="box__result--text">
-        Em 90 dias: <strong>R$ {data[90] || 0},00</strong>
+        Em 90 dias: <strong>R$ {data[90] ? Number((data[90])).toLocaleString('pt-br') : 0},00</strong>
       </p>
     </div>
   );
