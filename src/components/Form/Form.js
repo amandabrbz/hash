c45d118eca3ae3213b788f6d89730c80037fb24a
Amanda Barboza <amanda.barboza@mirumagency.com>
2021-01-05 00:24:01 -0300

    arruma bugs

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,6 +9,7 @@ const Form = () => {
   const { setData } = useContext(Context);
 
   const [showInfo, setShowInfo] = useState(false);
+  const [error, setError] = useState({});
   const [forms, setForms] = useState({
     amount: 0,
     installments: "",
@@ -22,11 +23,45 @@ const Form = () => {
     // eslint-disable-next-line
   }, [forms]);
 
-  function handleChange({ target }, maskValue) {
+  function handleChange({ target }) {
     let { name, value } = target;
 
-    if (name === "amount") value = maskValue;
+    if (name === "amount") {
+      return value.replaceAll('.', '').replaceAll(',', '').replace('R$ ', '')
+    }
     setForms({ ...forms, [name]: value });
+  }
+
+  function handleOnBlur() {
+    setError(validate(forms));
+  }
+
+  function validate(values) {
+    let errors = {};
+
+    if (values.installments > 12) {
+      errors.installments = "Não pode ser maior que 12 parcelas";
+    } else if (values.installments <= 0) {
+      errors.installments = "Não pode ser menor que 0 parcelas";
+    } else if (!values.installments) {
+      errors.installments = "Não pode estar em branco";
+    }
+
+    if (values.amount > 1000000000) {
+      errors.amount = "Não pode ser maior que cem milhões de reais";
+    } else if (values.amount <= 0) {
+      errors.amount = "Não pode ser menor que zero reais";
+    } else if (!values.amount) {
+      errors.amount = "Não pode estar em branco";
+    }
+
+    if (values.mdr <= 0) {
+      errors.mdr = "Não pode ser menor que zero";
+    } else if (!values.mdr) {
+      errors.mdr = "Não pode estar em branco";
+    }
+
+    return errors;
   }
 
   async function handlePOST(forms) {
@@ -36,11 +71,10 @@ const Form = () => {
       const response = await fetch(url, options);
       const json = await response.json();
 
-      setData({ ...json });
+      console.log(forms);
+      console.log(json);
 
-      if (response.status === 400) {
-        alert(json.message);
-      }
+      setData({ ...json });
     } catch (error) {
       alert("Não foi possível realizar o cálculo tente novamente.");
       console.error(error);
@@ -61,10 +95,13 @@ const Form = () => {
               id="amount"
               value={forms.amount}
               onChange={handleChange}
+              onBlur={handleOnBlur}
               placeholder="R$ 0,00"
+              className={error.amount && "error"}
               autoFocus
               required
             />
+            {error.amount && <small className="error">{error.amount}</small>}
           </fieldset>
           <fieldset>
             <label htmlFor="installments">Em quantas parcelas*</label>
@@ -78,10 +115,15 @@ const Form = () => {
               placeholder="0x"
               value={forms.installments}
               onChange={handleChange}
+              onBlur={handleOnBlur}
               onFocus={() => setShowInfo(true)}
+              className={error.installments && "error"}
               required
             />
             {showInfo && <small>Máximo de 12 parcelas</small>}
+            {error.installments && (
+              <small className="error">{error.installments}</small>
+            )}
           </fieldset>
           <fieldset>
             <label htmlFor="mdr">Informe o percentual de MDR*</label>
@@ -89,14 +131,16 @@ const Form = () => {
               type="number"
               name="mdr"
               id="mdr"
-              className="mdr"
               min="0"
               step="0.1"
               placeholder="0%"
               value={forms.mdr}
               onChange={handleChange}
+              onBlur={handleOnBlur}
+              className={error.mdr ? "error mdr" : "mdr"}
               required
             />
+            {error.mdr && <small className="error">{error.mdr}</small>}
           </fieldset>
         </form>
       </div>
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,28 +1,36 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./Results.css";
-import { Context } from '../Box/Box';
+import { Context } from "../Box/Box";
 
 const Results = () => {
-  const {data} = useContext(Context);
+  const { data } = useContext(Context);
 
   function formatString(value) {
-    return Number(value).toLocaleString('pt-br')
+    let cleanValue = value + "";
+    cleanValue = cleanValue.replace(/([0-9]{2})$/g, ",$1");
+    if (cleanValue.length > 6) cleanValue = cleanValue.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
+
+    return cleanValue;
   }
 
   return (
     <div className="box__result">
       <h2 className="box__result--title">Você receberá:</h2>
       <p className="box__result--text">
-        Amanhã: <strong>R$ {data[1] ? formatString(data[1]) : 0},00</strong>
+        Amanhã:{" "}
+        <strong>{data[1] ? "R$ " + formatString(data[1]) : "R$ 0,00"}</strong>
       </p>
       <p className="box__result--text">
-        Em 15 dias: <strong>R$ {data[15] ? formatString(data[15]) : 0},00</strong>
+        Em 15 dias:{" "}
+        <strong>{data[15] ? "R$ " + formatString(data[15]) : "R$ 0,00"}</strong>
       </p>
       <p className="box__result--text">
-        Em 30 dias: <strong>R$ {data[30] ? formatString(data[30]) : 0},00</strong>
+        Em 30 dias:{" "}
+        <strong>{data[30] ? "R$ " + formatString(data[30]) : "R$ 0,00"}</strong>
       </p>
       <p className="box__result--text">
-        Em 90 dias: <strong>R$ {data[90] ? formatString(data[90]) : 0},00</strong>
+        Em 90 dias:{" "}
+        <strong>{data[90] ? "R$ " + formatString(data[90]) : "R$ 0,00"}</strong>
       </p>
     </div>
   );
