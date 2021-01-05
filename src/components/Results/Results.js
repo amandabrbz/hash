9e346bb3a6f457bf410b17c6ad56c0a1af63df1d
Amanda Barboza <amanda.barboza@mirumagency.com>
2021-01-04 22:02:15 -0300

    ajusta condicionais e faz animacao para aparecer resultado conforme o dia

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,29 +1,42 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./Results.css";
-import { Context } from '../Box/Box';
+import { Context } from "../Box/Box";
 
 const Results = () => {
-  const {data} = useContext(Context);
+  const { data } = useContext(Context);
 
   function formatString(value) {
-    return Number(value).toLocaleString('pt-br')
+    return Number(value).toLocaleString("pt-br");
   }
 
   return (
     <div className="box__result">
       <h2 className="box__result--title">Você receberá:</h2>
-      <p className="box__result--text">
-        Amanhã: <strong>R$ {data[1] ? formatString(data[1]) : 0},00</strong>
-      </p>
-      <p className="box__result--text">
-        Em 15 dias: <strong>R$ {data[15] ? formatString(data[15]) : 0},00</strong>
-      </p>
-      <p className="box__result--text">
-        Em 30 dias: <strong>R$ {data[30] ? formatString(data[30]) : 0},00</strong>
-      </p>
-      <p className="box__result--text">
-        Em 90 dias: <strong>R$ {data[90] ? formatString(data[90]) : 0},00</strong>
-      </p>
+
+      {data[1] && (
+        <p className="box__result--text">
+          Amanhã: <strong>R$ {formatString(data[1])},00</strong>
+        </p>
+      )}
+      {data[15] && (
+        <p className="box__result--text">
+          Em 15 dias:
+          <strong>R$ {formatString(data[15])},00</strong>
+        </p>
+      )}
+      {data[30] && (
+        <p className="box__result--text">
+          Em 30 dias:
+          <strong>R$ {formatString(data[30])},00</strong>
+        </p>
+      )}
+      {data[90] && (
+        <p className="box__result--text">
+          Em 90 dias:
+          <strong>R$ {formatString(data[90])},00</strong>
+        </p>
+      )}
+      
     </div>
   );
 };
