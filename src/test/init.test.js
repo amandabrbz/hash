00498dbc64152beca405220a384bf60758e91d40
amandabrbz <amanda.brbz@hotmail.com>
2021-01-23 10:55:15 -0300

    corrige init test

--- src/test/init.test.js
+++ src/test/init.test.js
@@ -1,14 +1,15 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import '@testing-library/jest-dom'
 import App from "../App";
 
 describe("first rendering", () => {
-  test("should render box", () => {
+  it("should render box", () => {
     render(<App />);
     expect(
       screen.getByRole("heading", {
         name: /simule sua antecipação/i,
       })
-    );
+    ).toBeInTheDocument();
   });
 });
